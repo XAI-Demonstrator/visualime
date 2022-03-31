8b85afe29af7e138ae531bf4fdfa61c1c7dbdff2
Kilian Kluge <dev@kluge.ai>
2022-03-31 12:47:47 +0200

    Pythonify LIME implementation

--- visualime/explain.py
+++ visualime/explain.py
@@ -9,7 +9,7 @@ from .lime import create_segments, generate_samples, generate_images, predict_im
 def explain_image(img: np.ndarray, seg_method: str, seg_settings: Dict, num_of_samples: int, samples_p: float,
                   model_, threshold: float, volume: int, colour: str,
                   transparency: float) -> np.ndarray:
-    segment_mask = create_segments(img=img, seg_method=seg_method, settings=seg_settings)
+    segment_mask = create_segments(img=img, segmentation_method=seg_method, segmentation_settings=seg_settings)
     samples_theo = generate_samples(segment_mask=segment_mask, num_of_samples=num_of_samples, p=samples_p)
     samples_imgs = generate_images(image=img, segment_mask=segment_mask, samples=samples_theo)
     samples_imgs_predictions = predict_images(images=samples_imgs, model_=model_)
