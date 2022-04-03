50db497d881ae95692d1f0fba4edeb8bbf30921a
Kilian Kluge <dev@kluge.ai>
2022-04-03 14:12:20 +0200

    Add end-to-end test based on example

---
+++ tests/test_explain.py
@@ -0,0 +1,45 @@
+from urllib.request import urlopen
+
+import numpy as np
+from PIL import Image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+
+from visualime.explain import explain_classification, render_explanation
+
+model = MobileNetV2()
+
+
+def predict_fn(image):
+    return model.predict(preprocess_input(image))
+
+
+FULL_IMAGE = Image.open(
+    urlopen(
+        "https://storage.googleapis.com/xai-demo-assets/visual-inspection/images/table.jpg"
+    )
+)
+
+
+def test_end_to_end():
+    camera_img = FULL_IMAGE.crop((766, 90, 990, 314))
+
+    image = np.array(camera_img)
+    assert image.shape == (224, 224, 3)
+
+    prediction = predict_fn(image[None, :, :, :])
+    assert np.argmax(prediction, axis=1)[0] == 759
+
+    segment_mask, segment_weights = explain_classification(
+        image=image, predict_fn=predict_fn, num_of_samples=128
+    )
+
+    assert segment_mask.shape == (224, 224)
+
+    _ = render_explanation(
+        image,
+        segment_mask,
+        segment_weights,
+        positive="green",
+        negative="red",
+        coverage=0.2,
+    )
--- tests/test_lime.py
+++ tests/test_lime.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from visualime.lime import generate_images, generate_samples
+from visualime.lime import generate_images, generate_samples, image_distances
 
 
 def test_that_replacing_background_with_image_generates_original_image():
@@ -14,3 +14,17 @@ def test_that_replacing_background_with_image_generates_original_image():
 
     for img_idx in range(images.shape[0]):
         assert (images[img_idx, :, :, :] == image).all()
+
+
+def test_that_image_distances_are_computed():
+    img_size_x, img_size_y = (224, 224)
+
+    image = np.random.randint(0, 255, size=(img_size_x, img_size_y, 3))
+    segment_mask = np.random.randint(0, 25, size=(img_size_x, img_size_y))
+    samples = generate_samples(segment_mask, 100, p=0.5)
+
+    images = generate_images(image, segment_mask, samples, background=image)
+
+    distances = image_distances(image, images)
+
+    assert distances.shape == (100,)
