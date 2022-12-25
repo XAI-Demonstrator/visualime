a33ce5fd2c85920c567e3177a5a834b6a92eb4e8
Kilian Kluge <dev@kluge.ai>
2022-12-25 15:01:40 +0100

    test: Fully cover all explanation-generating functions

--- tests/test_explain.py
+++ tests/test_explain.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 
 import numpy as np
+import pytest
 from PIL import Image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
 
@@ -19,20 +20,22 @@ FULL_IMAGE = Image.open(
     )
 )
 
+camera_img = FULL_IMAGE.crop((766, 90, 990, 314))
 
-def test_end_to_end():
-    camera_img = FULL_IMAGE.crop((766, 90, 990, 314))
+image = np.array(camera_img)
+assert image.shape == (224, 224, 3)
 
-    image = np.array(camera_img)
-    assert image.shape == (224, 224, 3)
+prediction = predict_fn(image[None, :, :, :])
+assert np.argmax(prediction, axis=1)[0] == 759
 
-    prediction = predict_fn(image[None, :, :, :])
-    assert np.argmax(prediction, axis=1)[0] == 759
 
+@pytest.mark.parametrize("segment_selection_method", ["forward_selection", "by_weight"])
+def test_end_to_end(segment_selection_method):
     segment_mask, segment_weights = explain_classification(
         image=image,
         predict_fn=predict_fn,
         num_of_samples=128,
+        segment_selection_method=segment_selection_method,
     )
 
     assert segment_mask.shape == (224, 224)
@@ -45,3 +48,13 @@ def test_end_to_end():
         negative="red",
         coverage=0.2,
     )
+
+
+def test_that_unknown_selection_method_raises_exception():
+    with pytest.raises(ValueError):
+        _, _ = explain_classification(
+            image=image,
+            predict_fn=predict_fn,
+            num_of_samples=1,
+            segment_selection_method="this-method-does-not-exist",
+        )
--- tests/test_lime.py
+++ tests/test_lime.py
@@ -1,30 +1,93 @@
-import numpy as np
+import itertools
 
-from visualime.lime import compute_distances, generate_images, generate_samples
+import numpy as np
+import pytest
+
+from visualime.lime import (
+    SEGMENTATION_METHODS,
+    compute_distances,
+    create_segments,
+    generate_images,
+    generate_samples,
+    weigh_segments,
+)
+
+MAX_SEGMENT_INDEX = 25
+
+img_size_x, img_size_y = (224, 224)
+
+image = np.random.randint(0, 255, size=(img_size_x, img_size_y, 3))
+segment_mask = np.random.randint(0, MAX_SEGMENT_INDEX, size=(img_size_x, img_size_y))
+samples = generate_samples(segment_mask, 100, p=0.5)
+
+
+def test_that_create_segments_only_accepts_known_methods():
+    with pytest.raises(ValueError):
+        _ = create_segments(
+            image=image, segmentation_method="this-method-does-not-exist-and-never-will"
+        )
+
+
+@pytest.mark.parametrize("segmentation_method", list(SEGMENTATION_METHODS.keys()))
+def test_image_segmentation(segmentation_method):
+    segment_mask = create_segments(image=image, segmentation_method=segmentation_method)
+
+    assert np.min(segment_mask) == 0
 
 
 def test_that_replacing_background_with_image_generates_original_image():
-    img_size_x, img_size_y = (128, 255)
-
-    image = np.random.randint(0, 255, size=(img_size_x, img_size_y, 3))
-    segment_mask = np.random.randint(0, 25, size=(img_size_x, img_size_y))
-    samples = generate_samples(segment_mask, 100, p=0.5)
-
     images = generate_images(image, segment_mask, samples, background=image)
 
     for img_idx in range(images.shape[0]):
         assert (images[img_idx, :, :, :] == image).all()
 
 
-def test_that_image_distances_are_computed():
-    img_size_x, img_size_y = (224, 224)
+@pytest.mark.parametrize(
+    "norm,select",
+    list(
+        itertools.product(
+            [None, "fro", "nuc", np.inf, -np.inf, 1, -1, 2, -2], ["sum", "max"]
+        )
+    ),
+)
+def test_that_image_distances_are_computed(norm, select):
+    images = generate_images(image, segment_mask, samples, background=1)
 
-    image = np.random.randint(0, 255, size=(img_size_x, img_size_y, 3))
-    segment_mask = np.random.randint(0, 25, size=(img_size_x, img_size_y))
-    samples = generate_samples(segment_mask, 100, p=0.5)
-
-    images = generate_images(image, segment_mask, samples, background=image)
-
-    distances = compute_distances(image, images)
+    distances = compute_distances(image, images, norm=norm, select=select)
 
     assert distances.shape == (100,)
+
+
+def test_that_invalid_select_method_raises_exception():
+    images = generate_images(image, segment_mask, samples, background=1)
+
+    with pytest.raises(ValueError):
+        _ = compute_distances(image, images, select="this-method-does-not-exist")
+
+
+def test_that_weigh_segments_only_accepts_known_models():
+    with pytest.raises(ValueError):
+        _ = weigh_segments(
+            samples=samples,
+            predictions=np.zeros((samples.shape[0], 10)),
+            label_idx=0,
+            model_type="this-model-does-not-exist-and-never-will",
+        )
+
+
+def test_segment_subset_validation_in_weigh_segments():
+    with pytest.raises(ValueError):
+        _ = weigh_segments(
+            samples=samples,
+            predictions=np.zeros((samples.shape[0], 5)),
+            label_idx=0,
+            segment_subset=[-5, 0, 1],
+        )
+
+    with pytest.raises(ValueError):
+        _ = weigh_segments(
+            samples=samples,
+            predictions=np.zeros((samples.shape[0], 5)),
+            label_idx=0,
+            segment_subset=[0, 1, 2, MAX_SEGMENT_INDEX + 1],
+        )
