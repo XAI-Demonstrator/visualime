7bc8e1eee4fb993a85936fd6b6bfd47b4beb6170
Kilian Kluge <dev@kluge.ai>
2022-12-26 23:14:38 +0100

    feat: Allow the user to pass parameters to linear models

--- visualime/_models.py
+++ visualime/_models.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict, Optional
+
 from sklearn.linear_model import BayesianRidge, Lasso, LinearRegression, Ridge
 
 LINEAR_MODELS = {
@@ -7,20 +9,29 @@ LINEAR_MODELS = {
     "linear_regression": LinearRegression,
 }
 
-MODEL_TYPE_DOC = """model_type : str
+MODEL_TYPE_PARAMS_DOC = f"""model_type : str
         The type of linear model to fit.
-        Available options are `"bayesian_ridge"`, `"lasso"`, and `"linear_regression"`.
+        Available options are: `"{'"`, `"'.join(list(LINEAR_MODELS.keys())[:-1])}"`,
+        and `"{list(LINEAR_MODELS.keys())[-1]}"`.
 
         See the `scikit-learn documentation
         <https://scikit-learn.org/stable/modules/classes.html#module-sklearn.linear_model>`_
-        for details on each of the methods."""
+        for details on each of the models.
+
+    model_params : dict, optional
+        Parameters to pass to the model during instantiation.
+
+        See the `scikit-learn documentation
+        <https://scikit-learn.org/stable/modules/classes.html#module-sklearn.linear_model>`_
+        for details on each of the models."""
 
 
-def instantiate_model(model_type):
+def instantiate_model(model_type: str, model_params: Optional[Dict[str, Any]] = None):
+    model_params = model_params or {}
+
     try:
-        linear_model = LINEAR_MODELS[model_type]()
+        return LINEAR_MODELS[model_type](**model_params)
     except KeyError:
         raise ValueError(
-            f"Unknown model_type '{model_type}'. Available options: {list(LINEAR_MODELS.keys())}"
+            f"Unknown model_type '{model_type}'. Available options: {', '.join(list(LINEAR_MODELS.keys()))}."
         )
-    return linear_model
