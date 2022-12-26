48e33614edb53cd1eb61459606d059efb510d2d9
Kilian Kluge <dev@kluge.ai>
2022-12-26 13:19:09 +0100

    fix(tests): Add tests for generate_overlay

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -1,7 +1,39 @@
 import numpy as np
 import pytest
 
-from visualime.visualize import _get_color
+from visualime.visualize import _get_color, generate_overlay
+
+
+def test_that_overlay_is_transparent_if_no_segments_are_colored():
+    segment_mask = np.zeros((128, 128), dtype=int)
+    overlay = generate_overlay(
+        segment_mask=segment_mask, segments_to_color=[], color="red", opacity=1.0
+    )
+
+    assert overlay.shape == (128, 128, 4)
+    assert np.all(overlay[:, :, 3] == 0)
+
+
+def test_that_selected_segments_are_colored():
+    segment_mask = np.zeros((128, 128), dtype=int)
+    segment_mask[5, 5:8] = 1
+    segment_mask[12:24, 12:24] = 2
+    segment_mask[99, 101] = 3
+
+    overlay = generate_overlay(
+        segment_mask=segment_mask, segments_to_color=[1, 3], color="blue", opacity=1.0
+    )
+
+    assert overlay.shape == (128, 128, 4)
+    assert np.any(overlay[:, :, 3] == 255)
+    # segment 1 is colored in blue
+    assert np.all(overlay[5, 5:8, 2:4] == 255)
+    assert np.all(overlay[5, 5:8, 0:2] == 0)
+    # segment 2 is not colored
+    assert np.all(overlay[12:24, 12:24] == 0)
+    # segment 3 is colored in blue
+    assert np.all(overlay[99, 101, 2:4] == 255)
+    assert np.all(overlay[99, 101, 0:2] == 0)
 
 
 @pytest.mark.parametrize(
