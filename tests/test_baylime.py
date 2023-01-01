a96303880c30def65ef24892873e52a5d190936f
Kilian Kluge <dev@kluge.ai>
2023-01-01 15:21:44 +0100

    docs: Add BayLIME to docs

--- tests/test_baylime.py
+++ tests/test_baylime.py
@@ -3,11 +3,11 @@ import pytest
 
 from visualime.baylime import BayesianRidgeFixedAlphaLambda, BayesianRidgeFixedLambda
 
+X = np.random.rand(100, 20)
+y = np.sum(X * np.random.rand(20), axis=1)
+
 
 def test_that_lambda_fixed():
-    X = np.random.rand(100, 20)
-    y = np.sum(X * np.random.rand(20), axis=1)
-
     model = BayesianRidgeFixedLambda(alpha_init=1.5, lambda_init=2.0)
 
     model.fit(X, y)
@@ -16,9 +16,6 @@ def test_that_lambda_fixed():
 
 
 def test_that_alpha_and_lambda_are_fixed():
-    X = np.random.rand(100, 20)
-    y = np.sum(X * np.random.rand(20), axis=1)
-
     model = BayesianRidgeFixedAlphaLambda(alpha_init=1_500, lambda_init=2.0)
 
     model.fit(X, y)
@@ -28,13 +25,26 @@ def test_that_alpha_and_lambda_are_fixed():
 
 
 @pytest.mark.parametrize(
-    "model,kwargs",
+    "model_class,kwargs",
     [
         (BayesianRidgeFixedLambda, {}),
         (BayesianRidgeFixedAlphaLambda, {"lambda_init": 5.0}),
         (BayesianRidgeFixedAlphaLambda, {"alpha_init": 0.44}),
     ],
 )
-def test_that_missing_priors_raise_exception(model, kwargs):
+def test_that_missing_priors_raise_exception(model_class, kwargs):
     with pytest.raises(ValueError):
-        _ = model(**kwargs)
+        _ = model_class(**kwargs)
+
+
+@pytest.mark.parametrize(
+    "model_class,kwargs",
+    [
+        (BayesianRidgeFixedLambda, {"lambda_init": 12.4}),
+        (BayesianRidgeFixedAlphaLambda, {"lambda_init": 5.0, "alpha_init": 0.44}),
+    ],
+)
+def test_that_changes_to_parameters_issue_warning(model_class, kwargs):
+    model = model_class(epsilon=0.0, large_number=1, **kwargs)
+    with pytest.warns(UserWarning):
+        model.fit(X, y)
