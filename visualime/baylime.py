97c4fdc7e772efc6f2fcc725f17304f0b2041127
Kilian Kluge <dev@kluge.ai>
2023-01-01 15:00:22 +0100

    fix: Test BayLIME models and adapt to upstream implementation

--- visualime/baylime.py
+++ visualime/baylime.py
@@ -26,17 +26,21 @@ class BayesianRidgeFixedLambda(BayesianRidge):
     of available parameters.
     """
 
-    def __init__(self, *, epsilon=1e-6, large_number=1e6, **kwargs):
+    def __init__(self, *, epsilon=1e-6, large_number=1e9, **kwargs):
         if "lambda_init" not in kwargs:
             raise ValueError("'lambda_init' must be set.")
-        super(BayesianRidgeFixedLambda, self).__init__(**kwargs)
+
+        super().__init__(**kwargs)
+
         self.epsilon = epsilon
         self.large_number = large_number
 
     def fit(self, X, y, sample_weight=None):
         self.lambda_1 = self.lambda_init * self.large_number
         self.lambda_2 = self.large_number
-        super(BayesianRidgeFixedLambda, self).fit(X, y, sample_weight)
+
+        super().fit(X, y, sample_weight)
+
         if abs(self.lambda_ - self.lambda_init) > self.epsilon:
             warn(
                 f"lambda changed by more than {self.epsilon:.2E}. See documentation for details.",
@@ -50,22 +54,51 @@ class BayesianRidgeFixedAlphaLambda(BayesianRidge):
     """BayesianRidge model with fixed parameters alpha and lambda.
     This is equivalent to the "full informative priors" option in the BayLIME framework.
 
-    The values for `alpha_init` and `lambda_init` will be used as the model's
-    parameters, no model selection is performed.
+    The values for `alpha_init` and `lambda_init` will be treated as constants.
+
+    The parameter `epsilon` specifies the maximum amount of change allowed
+    for `alpha` and `lambda`.
+    A warning will be issued if this amount is exceeded.
+    To reduce the amount of change, increase `large_number`.
 
     See the documentation for :meth:`sklearn.linear_model.BayesianRidge` for a list
     of available parameters.
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self, *, epsilon=1e-6, large_number=1e9, **kwargs):
         if "alpha_init" not in kwargs:
             raise ValueError("'alpha_init' must be set.")
         if "lambda_init" not in kwargs:
             raise ValueError("'lambda_init' must be set.")
-        super(BayesianRidgeFixedAlphaLambda, self).__init__(**kwargs)
+
+        super().__init__(**kwargs)
+
+        self.epsilon = epsilon
+        self.large_number = large_number
 
     def fit(self, X, y, sample_weight=None):
-        self.n_iter = 0
-        super(BayesianRidgeFixedAlphaLambda, self).fit(X, y, sample_weight)
+        # In principle, it would be sufficient to set self.n_iter = 0 to skip the convergence loop,
+        # but then we get an UnboundLocalError due to the implementation of fit()
+        self.n_iter = 1
+
+        self.lambda_1 = self.lambda_init * self.large_number
+        self.lambda_2 = self.large_number
+
+        self.alpha_1 = self.alpha_init * self.large_number
+        self.alpha_2 = self.large_number
+
+        super().fit(X, y, sample_weight)
+
+        if abs(self.lambda_ - self.lambda_init) > self.epsilon:
+            warn(
+                f"lambda changed by more than {self.epsilon:.2E}. See documentation for details.",
+                RuntimeWarning,
+            )
+
+        if abs(self.alpha_ - self.alpha_init) > self.epsilon:
+            warn(
+                f"lambda changed by more than {self.epsilon:.2E}. See documentation for details.",
+                RuntimeWarning,
+            )
 
     fit.__doc__ = BayesianRidge.fit.__doc__
